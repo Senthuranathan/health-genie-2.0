b80fc74a84b6321448b96b632bde707794297c03
Senthuranathan <senthuranathans@gmail.com>
2026-05-14 08:36:11 +0530

    Fix errors

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,3 @@ function App() {
 }
 
 export default App;
-
-git init
-git add .
-git commit -m "Initial commit"
-git branch -M main
-git remote add origin https://github.com/Senthuranathan/healthgenie2.0.git
-git push -u origin main
